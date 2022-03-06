e7885ce5e8bcb889eedcb33e495f2196b8e258f0
Adrien Bonnin <adbonnin@users.noreply.github.com>
2022-03-06 22:57:24 +0100

    :sparkles: Ajout des unités de durée

---
+++ kumoko-commons/src/main/java/fr/adbonnin/kumoko/time/DurationUtils.java
@@ -0,0 +1,14 @@
+package fr.adbonnin.kumoko.time;
+
+public class DurationUtils {
+
+    public static final long MILLIS_PER_SECOND = 1000;
+
+    public static final long MILLIS_PER_MINUTE = 60 * MILLIS_PER_SECOND;
+
+    public static final long MILLIS_PER_HOUR = 60 * MILLIS_PER_MINUTE;
+
+    public static final long MILLIS_PER_DAY = 24 * MILLIS_PER_HOUR;
+
+    private DurationUtils() { /* Cannot be instantiated */ }
+}
